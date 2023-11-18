2b6202859c981d01b562aec3a0aa4d30ceaf8691
Misha133 <mihagribkov133@gmail.com>
2023-11-19 01:27:46 +0300

    update to dotnet 8 + DNet 3.13

--- Activities Bot/Database/GuildLocale.cs
+++ Activities Bot/Database/GuildLocale.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks; 
 
 namespace Activities_Bot.Database;
 
@@ -18,6 +13,6 @@ public class GuildLocale
 
     [Column("Culture")]
     [DefaultValue("en-US")]
-    [MaxLength(10)]
+    [System.ComponentModel.DataAnnotations.MaxLength(10)]
     public string Culture { get; set; }
 }
